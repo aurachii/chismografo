18a2139b948d2e0e2dca4fac9f7d9f159d8e3292
Ibeth Rivero <47192437+aurachii@users.noreply.github.com>
2022-04-25 17:58:25 +0300

    Log the error message and exit

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ func home(w http.ResponseWriter, r *http.Request){
 }
 
 func main(){
-	mux:=http.NewServeMux()
+	mux := http.NewServeMux()
 	mux.HandleFunc("/",home)
 	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000",mux)
+	log.Fatal(err)
 }
