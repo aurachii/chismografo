90f0d8836bcc4c13c084b3d7d11b525ce6c5c509
Ibeth Rivero <47192437+aurachii@users.noreply.github.com>
2022-04-25 18:10:19 +0300

    Added Handler Function for Home Page

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
-package main 
+package main
 
-import {
+import (
 	"log"
 	"net/http"
-}
+)
 
-// Home Handler function (writes a byte slice containing a response body)
+// Handler Function for Home Page
 
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Eres bien chismoso"))
 }
 
-func main(){
+func main() {
+
+	//Initialize a new servemux
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/",home)
+
+	//Registering the home function as the handler for the "/" URL pattern
+
+	mux.HandleFunc("/", home)
+
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000",mux)
+
+	// Start a new web server
+
+	err := http.ListenAndServe(":4000", mux)
+
+	// Log error message and exit
+
 	log.Fatal(err)
 }
