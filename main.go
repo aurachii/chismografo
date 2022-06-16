c1709ae954302ac1a7cbe39c36060e8af0d79b64
Ibeth Rivero <47192437+aurachii@users.noreply.github.com>
2022-06-16 16:38:25 +0300

    Added new handlers, started server, initialized servemux

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
+// Import Packages 
+
 import (
 	"log"
 	"net/http"
 )
+
+// Define handlers
+
+// Response body handler (home page)
+
+func home(w http.ResponseWritter, r *http.Request){
+	w.Write([]byte("Bienvenido al Chismografo"))
+}
+
+func main(){
+
+	//Initialize a new servemux
+
+	mux:=http.NewServeMux()
+
+	//Register the home function as the handler for the "/" URL pattern
+
+	mux.HandleFunc("/",home)
+
+	// Start Server
+
+	log.Println("Starting server on :4000")
+	err:=http.ListenAndServe(":4000",mux)
+	log.Fatal(err)
+
+}
+
+
 
 
 
